e20a27ec93ceef9cca26de9c07e9b9c371410e82
KaizerHind <42819568+KaizerHind@users.noreply.github.com>
2023-11-14 18:19:48 -0500

    `EventGuilds`

---
+++ EventGuilds/GuildServer/ready.js
@@ -0,0 +1,30 @@
+const { ActivityType } = require('discord.js');
+
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute(client, message, args) {
+        let CantMembers = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0);
+
+
+        const activities = [
+            'Docs about SA-OP:MP 📖',
+            'My code and updating 📝',
+            'Docs about Discord v14. 📖',
+            'Repositories in Github. 📚',
+        ]
+
+        console.log("===================================================");
+        console.log(`# The Bot ${client.user.username}, is now connected.             #`)
+        console.log(`# ${client.user.username} - It has been executed satisfactorily! #`);
+        console.log("===================================================");
+
+        setInterval(() => {
+            client.user.setPresence({
+                status: 'dnd',
+                activities: [{ name: `${activities[Math.floor(Math.random() * activities.length)]}`, type: ActivityType.Watching }],
+            })
+        }, 7000)
+
+    },
+};
